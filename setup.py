c4fc932d1606ffbe59438dbe8c2bbf6314e205e1
David Wahlund <david@dafnet.se>
2019-09-23 17:53:35 +0200

    Added github url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='menu',
       version='190923.1',
-      url='',
+      url='https://github.com/dafvid/menu',
       author='David Wahlund',
       author_email='david@dafnet.se',
       scripts=['menu.py'],
