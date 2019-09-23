e06595b1459863b0a63e6fe4e7a5102fcd7e5849
DavidW <david@dafnet.se>
2019-09-23 17:24:22 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='menu',
+      version='190923.1',
+      url='',
+      author='David Wahlund',
+      author_email='david@dafnet.se',
+      scripts=['menu.py'],
+      )
